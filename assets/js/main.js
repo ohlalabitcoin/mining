28e55bb72982bb798652210a13c6b6f50b36fc6b
valentin <valentin.mebarki@yahoo.fr>
2018-04-19 18:28:55 +0200

    [WIP] adding mail form

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,18 +4,18 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
+(function ($) {
 
 	var settings = {
 
 		// Full screen header?
-			fullScreenHeader: true,
+		fullScreenHeader: true,
 
 		// Parallax background effect?
-			parallax: true,
+		parallax: true,
 
 		// Parallax factor (lower = more intense, higher = less intense).
-			parallaxFactor: 10
+		parallaxFactor: 10
 
 	};
 
@@ -26,9 +26,9 @@
 		mobile: '(max-width: 736px)'
 	});
 
-	$(function() {
+	$(function () {
 
-		var	$window = $(window),
+		var $window = $(window),
 			$body = $('body');
 
 		if (skel.vars.mobile) {
@@ -39,123 +39,123 @@
 		}
 
 		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+		$body.addClass('is-loading');
 
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
+		$window.on('load', function () {
+			$body.removeClass('is-loading');
+		});
 
 		// CSS polyfills (IE<9).
-			if (skel.vars.IEVersion < 9)
-				$(':last-child').addClass('last-child');
+		if (skel.vars.IEVersion < 9)
+			$(':last-child').addClass('last-child');
 
 		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+		$('form').placeholder();
 
 		// Prioritize "important" elements on mobile.
-			skel.on('+mobile -mobile', function() {
-				$.prioritize(
-					'.important\\28 mobile\\29',
-					skel.breakpoint('mobile').active
-				);
-			});
+		skel.on('+mobile -mobile', function () {
+			$.prioritize(
+				'.important\\28 mobile\\29',
+				skel.breakpoint('mobile').active
+			);
+		});
 
 		// Scrolly links.
-			$('.scrolly-middle').scrolly({
-				speed: 1000,
-				anchor: 'middle'
-			});
+		$('.scrolly-middle').scrolly({
+			speed: 1000,
+			anchor: 'middle'
+		});
 
-			$('.scrolly').scrolly({
-				speed: 1000,
-				offset: function() { return (skel.breakpoint('mobile').active ? 70 : 190); }
-			});
+		$('.scrolly').scrolly({
+			speed: 1000,
+			offset: function () { return (skel.breakpoint('mobile').active ? 70 : 190); }
+		});
 
 		// Full screen header.
-			if (settings.fullScreenHeader) {
+		if (settings.fullScreenHeader) {
 
-				var $header = $('#header');
+			var $header = $('#header');
 
-				if ($header.length > 0) {
+			if ($header.length > 0) {
 
-					var $header_header = $header.find('header');
+				var $header_header = $header.find('header');
 
-					$window
-						.on('resize.overflow_fsh', function() {
+				$window
+					.on('resize.overflow_fsh', function () {
 
-							if (skel.breakpoint('mobile').active)
-								$header.css('padding', '');
-							else {
+						if (skel.breakpoint('mobile').active)
+							$header.css('padding', '');
+						else {
 
-								var p = Math.max(192, ($window.height() - $header_header.outerHeight()) / 2);
-								$header.css('padding', p + 'px 0 ' + p + 'px 0');
+							var p = Math.max(192, ($window.height() - $header_header.outerHeight()) / 2);
+							$header.css('padding', p + 'px 0 ' + p + 'px 0');
 
-							}
+						}
 
-						})
-						.trigger('resize.overflow_fsh');
+					})
+					.trigger('resize.overflow_fsh');
 
-					$window.load(function() {
-						$window.trigger('resize.overflow_fsh');
-					});
-
-				}
+				$window.load(function () {
+					$window.trigger('resize.overflow_fsh');
+				});
 
 			}
+
+		}
 
 		// Parallax background.
 
-			// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
-				if (skel.vars.browser == 'ie'
-				||	skel.vars.mobile)
-					settings.parallax = false;
+		// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
+		if (skel.vars.browser == 'ie'
+			|| skel.vars.mobile)
+			settings.parallax = false;
 
-			if (settings.parallax) {
+		if (settings.parallax) {
 
-				var $dummy = $(), $bg;
+			var $dummy = $(), $bg;
 
-				$window
-					.on('scroll.overflow_parallax', function() {
+			$window
+				.on('scroll.overflow_parallax', function () {
 
-						// Adjust background position.
-							$bg.css('background-position', 'center ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
+					// Adjust background position.
+					$bg.css('background-position', 'center ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
 
-					})
-					.on('resize.overflow_parallax', function() {
+				})
+				.on('resize.overflow_parallax', function () {
 
-						// If we're in a situation where we need to temporarily disable parallax, do so.
-							if (!skel.breakpoint('wide').active
-							||	skel.breakpoint('narrow').active) {
+					// If we're in a situation where we need to temporarily disable parallax, do so.
+					if (!skel.breakpoint('wide').active
+						|| skel.breakpoint('narrow').active) {
 
-								$body.css('background-position', '');
-								$bg = $dummy;
+						$body.css('background-position', '');
+						$bg = $dummy;
 
-							}
+					}
 
-						// Otherwise, continue as normal.
-							else
-								$bg = $body;
+					// Otherwise, continue as normal.
+					else
+						$bg = $body;
 
-						// Trigger scroll handler.
-							$window.triggerHandler('scroll.overflow_parallax');
+					// Trigger scroll handler.
+					$window.triggerHandler('scroll.overflow_parallax');
 
-					})
-					.trigger('resize.overflow_parallax');
+				})
+				.trigger('resize.overflow_parallax');
 
-			}
+		}
 
 		// Poptrox.
-			$('.gallery').poptrox({
-				useBodyOverflow: false,
-				usePopupEasyClose: false,
-				overlayColor: '#0a1919',
-				overlayOpacity: (skel.vars.IEVersion < 9 ? 0 : 0.75),
-				usePopupDefaultStyling: false,
-				usePopupCaption: true,
-				popupLoaderText: '',
-				windowMargin: 10,
-				usePopupNav: true
-			});
+		$('.gallery').poptrox({
+			useBodyOverflow: false,
+			usePopupEasyClose: false,
+			overlayColor: '#0a1919',
+			overlayOpacity: (skel.vars.IEVersion < 9 ? 0 : 0.75),
+			usePopupDefaultStyling: false,
+			usePopupCaption: true,
+			popupLoaderText: '',
+			windowMargin: 10,
+			usePopupNav: true
+		});
 
 	});
 
